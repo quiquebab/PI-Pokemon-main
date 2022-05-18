96899a35445570e2c795faea1c6b5dc9866e8783
Your Name <you@example.com>
2022-05-18 19:44:37 -0300

    base de datos

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,48 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id:{
+      type:DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      primaryKey: true,
     },
+    life:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+    strength:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+   defending:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+    speed:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+    height:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+    weight:{
+      type:DataTypes.STRING,
+      allowNull: true,
+    },
+    createInDb: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+
+
+
   });
 };
